cdde7bf5687feac692a915b4b14d8aabdb5821e3
pomeran <lejk99@icloud.com>
2025-07-23 20:58:27 +0200

    Added functionality for downloading financial documents

--- business_data_project/business_details/views.py
+++ business_data_project/business_details/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.db.models import Q
 
+import io
+from zipfile import ZipFile
+
 from .models import CompanyInfo, KrsDfDocuments
 
 # Create your views here.
@@ -48,6 +51,29 @@ def documents(request, company_id):
     context = {}
     company = get_object_or_404(CompanyInfo.objects.using("data"), pk=company_id)
     context["company"] = company
+    if request.method == "POST":
+        selected_hash_ids = request.POST.getlist("doc_ids")
+        if selected_hash_ids:
+            documents = (KrsDfDocuments
+                            .objects
+                            .using("data")
+                            .filter(hash_id__in=selected_hash_ids)
+                            .values(
+                                "document_content_save_name",
+                                "document_content"))
+            zip_buffer = io.BytesIO()
+            with ZipFile(zip_buffer, "w") as zip_file:
+                for doc in documents:
+                    zip_file.writestr(
+                        doc["document_content_save_name"],
+                        doc["document_content"])
+            zip_buffer.seek(0)
+            response = HttpResponse(zip_buffer, content_type="application/zip")
+            response["Content-Disposition"] = 'attachment; filename="documents.zip"'
+            return response
+
+
+
     documents = (KrsDfDocuments
                            .objects
                            .using("data")
@@ -56,6 +82,9 @@ def documents(request, company_id):
                                "hash_id", 
                                "document_type", 
                                "document_date_from",
-                               "document_date_to"))
+                               "document_date_to",
+                               "document_content_save_name",))
     context["documents"] = documents
     return render(request, "business_details/documents.html", context)
+
+
